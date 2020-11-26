7828da6f7d31c17139c372cffd22de1952a0673d
william341 <william341@localhost.localdomain>
2020-11-25 21:06:23 -0700

    finish Components, General

--- src/planet/admin/AdminComponents.tsx
+++ src/planet/admin/AdminComponents.tsx
@@ -1,6 +1,10 @@
-import { Button, Classes, Icon, Popover } from "@blueprintjs/core";
+import { useMutation } from "@apollo/client";
+import { Button, Classes, Icon, Intent, Popover } from "@blueprintjs/core";
 import React, { useState } from "react";
+import removeComponentMutation, { IRemoveComponentMutationData } from "../../graphql/mutations/planets/removeComponentMutation";
+import renameComponentMutation, { IRenameComponentMutationData } from "../../graphql/mutations/planets/renameComponentMutation";
 import IPlanet from "../../types/IPlanet";
+import { GlobalToaster } from "../../util/GlobalToaster";
 import ComponentIndex from "../ComponentIndex";
 import "./css/AdminComponents.css";
 
@@ -10,6 +14,17 @@ interface IAdminComponentProps {
 
 function AdminComponent(props: IAdminComponentProps): JSX.Element {
   const [popoverId, setPopoverId] = useState<string>("");
+  const [renameComponent] = useMutation<IRenameComponentMutationData>(renameComponentMutation);
+  const [deleteComponent] = useMutation<IRemoveComponentMutationData>(removeComponentMutation);
+  const [nameTextbox, setNameTextbox] = useState<string>("");
+
+  const doRename = function(id: string) {
+    renameComponent({variables: {planetId: props.planet.id, componentId: id, name: nameTextbox}}).then(() => {
+      setPopoverId("");
+    }).catch((err: Error) => {
+      GlobalToaster.show({message: err.message, intent: Intent.DANGER});
+    });
+  };
 
   return (
     <div className="Admin bp3-dark">
@@ -21,14 +36,28 @@ function AdminComponent(props: IAdminComponentProps): JSX.Element {
               {props.planet.components?.map((value) => (
                 <tr key={value.componentId}>
                   <td className="AdminComponents-table-name"><Icon className="AdminComponents-table-name-icon" icon={ComponentIndex.ComponentDataTypes[value.type].icon}/> {value.name} <Popover isOpen={popoverId === value.componentId} onClose={() => setPopoverId("")}>
-                    <Icon className="AdminComponents-table-name-icon" icon="edit" onClick={() => setPopoverId(value.componentId)}/>
+                    <Icon className="AdminComponents-table-name-icon" icon="edit" onClick={() => {
+                      setPopoverId(value.componentId);
+                      setNameTextbox(value.name);
+                    }}/>
                     <div className="menu-form">
-                      <input className={Classes.INPUT + " menu-input"} onKeyDown={(e) => {e.key === "Enter" && setPopoverId("");}}/>
-                      <Button text="Rename" className="menu-button"/>
+                      <input
+                        className={Classes.INPUT + " menu-input"}
+                        onKeyDown={(e) => {e.key === "Enter" && doRename(value.componentId);}}
+                        onChange={(e) => setNameTextbox(e.target.value)}
+                        value={nameTextbox}
+                      />
+                      <Button text="Rename" className="menu-button" onClick={() => doRename(value.componentId)}/>
                     </div>
                   </Popover></td>
                   <td className="AdminComponents-table-action">
-                    <Button intent="danger" className="AdminComponents-action-button" small={true} icon="trash"/>
+                    <Button intent="danger" className="AdminComponents-action-button" small={true} icon="trash" onClick={() => {
+                      deleteComponent({variables: {planetId: props.planet.id, componentId: value.componentId}}).then(() => {
+                        GlobalToaster.show({message: `Sucessfully deleted ${value.name}.`, intent: Intent.SUCCESS});
+                      }).catch((err: Error) => {
+                        GlobalToaster.show({message: err.message, intent: Intent.DANGER});
+                      });
+                    }}/>
                     {/* ComponentIndex.ComponentDataTypes[value.type].settingsComponent? && <Button small={true} className="AdminComponents-action-button" icon="settings"/>*/}
                   </td>
                 </tr>
--- src/planet/admin/AdminGeneral.tsx
+++ src/planet/admin/AdminGeneral.tsx
@@ -2,6 +2,10 @@ import IPlanet from "../../types/IPlanet";
 import React, { useState } from "react";
 import { Button, Classes, ControlGroup, Intent, Label } from "@blueprintjs/core";
 import "./css/AdminGeneral.css";
+import { useMutation } from "@apollo/client";
+import updateNameMutation, { IUpdateNameMutationData } from "../../graphql/mutations/planets/updateNameMutation";
+import { GlobalToaster } from "../../util/GlobalToaster";
+import togglePrivateMutation, { ITogglePrivateMutationData } from "../../graphql/mutations/planets/togglePrivateMutation";
 
 interface IAdminGeneralProps {
   planet: IPlanet
@@ -9,6 +13,8 @@ interface IAdminGeneralProps {
 
 function AdminGeneral(props: IAdminGeneralProps): JSX.Element {
   const [nameTextboxContents, updateNameTextbox] = useState<string>(props.planet.name);
+  const [setName] = useMutation<IUpdateNameMutationData>(updateNameMutation);
+  const [togglePrivate] = useMutation<ITogglePrivateMutationData>(togglePrivateMutation);
 
   return (
     <div className="Admin bp3-dark">
@@ -19,10 +25,27 @@ function AdminGeneral(props: IAdminGeneralProps): JSX.Element {
             Name
             <ControlGroup className="dumb-workaround-fcg-padding">
               <input className={Classes.INPUT + " dumb-workaround-for-control-group"} value={nameTextboxContents} onChange={(e) => updateNameTextbox(e.target.value)} placeholder="Placeholder text" />
-              <Button text="Save"/>
+              <Button text="Save" onClick={() => {
+                setName({variables: {planetId: props.planet.id, name: nameTextboxContents}}).then(() => {
+                  GlobalToaster.show({message: "Succesfully changed name.", intent: Intent.SUCCESS});
+                }).catch((err: Error) => {
+                  GlobalToaster.show({message: err.message, intent: Intent.DANGER});
+                });
+              }}/>
             </ControlGroup>
           </Label>
-          <Button text={props.planet.private ? "Make this planet public" : "Make this planet private"} intent={Intent.DANGER} className="AdminGeneral-margin-button"/>
+          <Button 
+            text={props.planet.private ? "Make this planet public" : "Make this planet private"} 
+            intent={Intent.DANGER}
+            className="AdminGeneral-margin-button"
+            onClick={() => {
+              togglePrivate({variables: {planetId: props.planet.id}}).then((data) => {
+                GlobalToaster.show({message: `${props.planet.name} is now ${data.data?.togglePrivate.private ? "private" : "public"}.`, intent: Intent.SUCCESS});
+              }).catch((err: Error) => {
+                GlobalToaster.show({message: err.message, intent: Intent.DANGER});
+              });
+            }}
+          />
         </div>
       </div>
     </div>
