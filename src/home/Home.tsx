a64863c27b64e6a9b5535f6ee151da572a8cb248
william341 <wwokwilliam@gmail.com>
2021-01-25 20:18:01 -0700

    warning

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -22,9 +22,9 @@ function Home(): JSX.Element {
             Starship is in an early alpha stage. Expect bugs and unfinished features. (including this homepage, which will one day be more focused on your followed planets)<br/>
             If you find a bug, please report it <a href="https://starship.william341.me/planet/kCnATXqBCD4vEvzMB/pekuosDPGGxHKc6Qg">here</a>.
           </Callout>
-          <Callout icon="warning-sign" intent={Intent.WARNING} className="Home-alpha-callout-padtop">
-            EXPERIMENTAL BUILD! DO NOT SHIP!
-            This build is meant to connect to a GraphQL endpoint that is currently not publicly available, and it will not work.
+          <Callout icon="warning-sign" intent={Intent.WARNING} className="Home-alpha-callout-padtop" title="Expiremental version">
+            starship-client v0.5<br/>
+            <Link to="/planet/9kauFkf5iPb64jqKT/YKwFEqCtYXfD9RBGi">For more information, click here.</Link>
           </Callout>
           {process.env.NODE_ENV === "development" && <Callout icon="warning-sign" intent={Intent.WARNING} className="Home-alpha-callout-padtop">
             This is not a production build. You may encounter performance issues.
