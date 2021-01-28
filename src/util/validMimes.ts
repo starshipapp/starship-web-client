8869114d6c2b92bf49e957f1071b167ef780942e
william341 <wwokwilliam@gmail.com>
2021-01-27 21:04:14 -0700

    validMimes bugfix\

--- src/util/validMimes.ts
+++ src/util/validMimes.ts
@@ -1,6 +1,6 @@
 const MimeTypes = {
   imageTypes: ["image/apng", "image/bmp", "image/gif", "image/x-icon", "image/jpeg", "image/png", "image/webp"],
-  audioTypes: ["audio/webm", "audio/wave", "audio/wav", "audio/ogg", "audio/opus", "audio/mpeg", "audio/flac", "audio/aac"],
+  audioTypes: ["audio/webm", "audio/wave", "audio/wav", "audio/ogg", "audio/opus", "audio/mpeg", "audio/flac", "audio/aac", "audio/x-wav", "audio/wave", "audio/x-flac", "audio/x-ogg", "audio/x-mpeg"],
   videoTypes: ["video/x-msvideo", "video/mpeg", "video/ogg", "video/mp4", "video/webm", "video/3gpp", "video/quicktime"],
   previewTypes: [""]
 };
